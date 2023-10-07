2fb7f40dcdad6ec9fa65d525c14334ecd4eb36de
DaiyaanMuhammad <daiyaan.villain@gmail.com>
2023-10-07 22:56:30 +0600

    I don't know what I changed here

--- src/main/java/com/fynanse/Fynanse/api/models/User.java
+++ src/main/java/com/fynanse/Fynanse/api/models/User.java
@@ -5,49 +5,22 @@ import jakarta.persistence.*;
 @Entity
 @Table(name="USER")
 public class User {
-    private int id;
     @Id
     @Column(nullable = false, unique = true)
     private String username;
-    @Column(nullable = false, unique = true)
-    private String email;
     private String userPassword;
-    public String firstName;
-    public String lastName;
-
     public User(){
         System.out.println("User object created");
     }
-    public User(String username, String email, String password, String firstName, String lastName) {
+    public User(String username,  String password) {
         this.username = username;
-        this.email = email;
         this.userPassword = password;
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-    public String getFirstName() {
-        return firstName;
-    }
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-    public String getLastName() {
-        return lastName;
-    }
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
     }
     public String getUsername() {
         return username;
     }
     public void setUsername(String username) {
         this.username = username;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
     }
     public String getUserPassword() {
         return userPassword;
