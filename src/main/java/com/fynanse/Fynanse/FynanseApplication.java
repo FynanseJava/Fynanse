614c8bd74cdbf933e5e4e8b0522897f53c19e86a
DaiyaanMuhammad <daiyaan.villain@gmail.com>
2023-10-07 22:55:51 +0600

    Added view resolver for thymeleaf

--- src/main/java/com/fynanse/Fynanse/FynanseApplication.java
+++ src/main/java/com/fynanse/Fynanse/FynanseApplication.java
@@ -2,13 +2,26 @@ package com.fynanse.Fynanse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistration;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.spring6.view.ThymeleafViewResolver;
+
+import java.nio.charset.StandardCharsets;
 
 @SpringBootApplication
 public class FynanseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FynanseApplication.class, args);
+	}
+	@Bean
+	public ThymeleafViewResolver viewResolver(SpringTemplateEngine templateEngine){
+		ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+		viewResolver.setContentType("application/json");
+		viewResolver.setCharacterEncoding(StandardCharsets.UTF_8.name());
+		viewResolver.setOrder(1);
+		viewResolver.setViewNames(new String[] {"*.json"});
+		viewResolver.setTemplateEngine(templateEngine);
+		return viewResolver;
 	}
 }
