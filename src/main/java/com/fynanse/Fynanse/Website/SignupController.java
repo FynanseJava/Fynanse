2ef53b854b62e72eb3b00be4fc858fd353556b7a
DaiyaanMuhammad <daiyaan.villain@gmail.com>
2023-10-07 22:57:28 +0600

    Added models of thymeleaft to signup controller

--- src/main/java/com/fynanse/Fynanse/Website/SignupController.java
+++ src/main/java/com/fynanse/Fynanse/Website/SignupController.java
@@ -1,8 +1,33 @@
 package com.fynanse.Fynanse.Website;
 
+import com.fynanse.Fynanse.api.models.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class SignupController {
+    @GetMapping("/")
+    public String showHome(){
+        boolean isUserSignedIn = false;
+        if(isUserSignedIn){
+            return "home";
+        }
+        else{
+            return "login";
+        }
+    }
+    @GetMapping("/signup")
+    public String showSignUp(Model model){
+        User user = new User();
+        model.addAttribute("user", user);
+        return "signup";
+    }
 
+    @GetMapping("/signup_success")
+    public String showSuccess(){
+        return "signup_success";
+    }
 }
