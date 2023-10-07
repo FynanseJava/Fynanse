e1b52165fce95576fe88434d25ffbee9441f8870
DaiyaanMuhammad <daiyaan.villain@gmail.com>
2023-10-07 22:56:18 +0600

    changes to the POST mapping to work with formdata and thymleaf

--- src/main/java/com/fynanse/Fynanse/api/controllers/UserController.java
+++ src/main/java/com/fynanse/Fynanse/api/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.fynanse.Fynanse.api.controllers;
 import com.fynanse.Fynanse.api.models.User;
 import com.fynanse.Fynanse.api.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,9 +23,13 @@ public class UserController {
     public Optional<User> getUserByID(@PathVariable String username){
         return userService.getUserById(username);
     }
-    @RequestMapping(method=RequestMethod.POST, value="/api/users")
-    public void addUser(@RequestBody User user){
+    @RequestMapping(path = "/api/users",
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
+            method = RequestMethod.POST
+    )
+    public String addUser(@ModelAttribute("user") User user){
         userService.addUser(user);
+        return "signup_success";
     }
 
     @RequestMapping(method=RequestMethod.PUT, value="/api/users/{username}")
