c114f21bb4ce9f48c1abd55dd009bb78a97dbcf3
DaiyaanMuhammad <daiyaan.villain@gmail.com>
2023-10-10 17:19:33 +0600

    Getting the currentUser by their loggedIn status and modeling it

--- src/main/java/com/fynanse/Fynanse/Website/DashboardController.java
+++ src/main/java/com/fynanse/Fynanse/Website/DashboardController.java
@@ -1,14 +1,21 @@
 package com.fynanse.Fynanse.Website;
 
+import com.fynanse.Fynanse.api.models.User;
+import com.fynanse.Fynanse.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import java.util.Optional;
 
 @Controller
 public class DashboardController {
+    @Autowired
+    private UserService userService;
     @GetMapping("/")
     public String showHome(){
-        boolean isUserSignedIn = false;
-        if(isUserSignedIn){
+        Optional<User> currentUser = userService.getCurrentUser();
+        if(currentUser.get().loggedIn){
             return "redirect:/dashboard";
         }
         else{
@@ -16,7 +23,9 @@ public class DashboardController {
         }
     }
     @GetMapping("/dashboard")
-    public String showDashboard(){
+    public String showDashboard(Model model){
+        Optional<User> currentUser = userService.getCurrentUser();
+        model.addAttribute("currentUser", currentUser.get());
         return "dashboard";
     }
 }
