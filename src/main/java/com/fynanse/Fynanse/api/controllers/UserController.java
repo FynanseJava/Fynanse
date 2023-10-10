c49742a6d303284a66fc89d26f1e70c17d770ec8
DaiyaanMuhammad <daiyaan.villain@gmail.com>
2023-10-10 17:15:41 +0600

    UserController is now a REST api again. Login post mapping is moved to loginController

--- src/main/java/com/fynanse/Fynanse/api/controllers/UserController.java
+++ src/main/java/com/fynanse/Fynanse/api/controllers/UserController.java
@@ -3,14 +3,12 @@ package com.fynanse.Fynanse.api.controllers;
 import com.fynanse.Fynanse.api.models.User;
 import com.fynanse.Fynanse.api.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
-@Controller
+@RestController
 public class UserController {
     @Autowired
     private UserService userService;
@@ -19,25 +17,18 @@ public class UserController {
     public List<User> getAllUsers(){
         return userService.getAllUsers();
     }
-
     @GetMapping("/api/users/{username}")
     public Optional<User> getUserByID(@PathVariable String username){
         return userService.getUserById(username);
     }
-    @RequestMapping(path = "/api/users",
-            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
-            method = RequestMethod.POST
-    )
-    public String addUser(@ModelAttribute("user") User user){
+    @RequestMapping(path = "/api/users", method = RequestMethod.POST)
+    public void addUser(@ModelAttribute("user") User user){
         userService.addUser(user);
-        return "redirect:/login";
     }
-
     @RequestMapping(method=RequestMethod.PUT, value="/api/users/{username}")
     public void updateUser(@RequestBody User user, @PathVariable String username){
         userService.updateUser(username, user);
     }
-
     @RequestMapping(method=RequestMethod.DELETE, value="/api/users/{username}")
     public void deleteUser(@PathVariable String username){
         userService.deleteUser(username);
