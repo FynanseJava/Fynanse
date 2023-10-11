87ab82d15c20eb4d5976b13e57624aa73ab0eebc
DaiyaanMuhammad <daiyaan.villain@gmail.com>
2023-10-12 01:46:44 +0600

    Added Account class for one to one mapping to User

---
+++ src/main/java/com/fynanse/Fynanse/api/models/Account.java
@@ -0,0 +1,28 @@
+package com.fynanse.Fynanse.api.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Entity
+@Table(name = "ACCOUNT")
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @Column(columnDefinition = "DECIMAL(10,2) DEFAULT 0.0")
+    private double currentBalance;
+    @Column(columnDefinition = "DECIMAL(10,2) DEFAULT 0.0")
+    private double lastSpent;
+    @Column(columnDefinition = "DECIMAL(10,2) DEFAULT 0.0")
+    private double weeklySpent;
+    @Column(columnDefinition = "DECIMAL(10,2) DEFAULT 0.0")
+    private double monthlySpent;
+    @OneToOne(cascade = CascadeType.ALL, mappedBy = "account")
+    private User user;
+
+}
