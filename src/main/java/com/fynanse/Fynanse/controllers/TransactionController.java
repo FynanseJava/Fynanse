38cbb880088f2a1c8ea30119a29bccf75728aa91
Daiyaan Muhammad <daiyaan.villain@gmail.com>
2023-10-18 19:34:29 +0600

    Controls history page to return set of transactions

---
+++ src/main/java/com/fynanse/Fynanse/controllers/TransactionController.java
@@ -0,0 +1,30 @@
+package com.fynanse.Fynanse.controllers;
+
+import com.fynanse.Fynanse.models.User;
+import com.fynanse.Fynanse.services.TransactionService;
+import com.fynanse.Fynanse.services.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Optional;
+
+@Controller
+public class TransactionController {
+    @Autowired
+    private TransactionService transactionService;
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/seeHistory")
+    public String showHistory(HttpServletRequest request, Model model){
+        Optional<User> currentUser = userService.getCurrentUser(request);
+        if(currentUser.isEmpty()){
+            return "redirect:/login";
+        }
+        model.addAttribute("transactionSet", transactionService.getAllTransactions(currentUser.get().getUsername()));
+        return "history";
+    }
+}
